0f63000cf8ebcdf563310f8ffd57892322dc66ad
Michael Wettstein <chischte@gmx.ch>
2020-06-02 15:52:26 +0200

    Create PlatformIO project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Insomnia.h>
+
+// INPUT_PIN_UPPER_MOTOR
+
+// Debounce
+// Insomnia print_delay;
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("EXIT SETUP");
+}
+
+void loop() {}
